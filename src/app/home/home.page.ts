1c0df014bff56d0ebd1a3e32ed94614dcd04d6c8
Rizki Aryandi <aryandi30@gmail.com>
2021-01-05 18:37:29 -0800

    v1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,70 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  dataPasien;
+  dataPasienDef;
 
+  homePage = {
+    loadPage:{
+      visibility:"visible"
+    },
+    tabelData:{
+      visibility:"hidden"
+    }
+  }
+
+  constructor(private http:HttpClient) {}
+
+  ngOnInit(){
+    this.getDataPasien();
+  }
+
+  getDataPasien(){
+    this.homePage.loadPage.visibility = "visible";
+    this.homePage.tabelData.visibility = "hidden";
+    this.http.get("https://uas-ram.xyz/ganjil/DataPasien").subscribe(data=>{
+      this.homePage.loadPage.visibility = "hidden";
+      this.homePage.tabelData.visibility = "visible";
+      this.dataPasien = data;
+      this.dataPasien = this.dataPasien.DataPasien;
+      this.dataPasienDef = this.dataPasien;
+    });
+  }
+
+  doRefresh(event) {
+    this.getDataPasien();
+
+    setTimeout(() => {
+      event.target.complete();
+    }, 100);
+  }
+
+
+  search(key){
+    var dataSeacrh = this.dataPasienDef.filter(obj => obj.nama.toLowerCase().indexOf(key.toLowerCase()) >= 0);
+    if(dataSeacrh == []){
+      this.dataPasien = this.dataPasienDef;
+    }
+    else{
+      this.dataPasien = dataSeacrh;
+    }
+  }
+
+  sort(ev){
+    if(ev == "id") this.dataPasien.sort(this.dynamicSort("no_rm"));
+    if(ev == "nama") this.dataPasien.sort(this.dynamicSort("nama"));
+    if(ev == "alamat") this.dataPasien.sort(this.dynamicSort("alamat"));
+  }
+
+  dynamicSort(property) {
+    var sortOrder = 1;
+    if(property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+    return function (a,b) {
+        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
+    }
+  }
 }
